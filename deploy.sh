9fa1712c91b8d3bb437b89a7e7265e0d92ff9748
Mark Arciaga <mark.arciaga@gmail.com>
2017-05-14 10:03:13 -0700

    Improve deploy script to indicate stages

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 ssh $REMOTE_IP << EOF
+echo "Change to working directory"
 cd $APP_DIR
+echo "Fetch current repository"
 git pull origin master
+echo "Install dependencies"
 yarn
+echo "Create temporary password-file"
 touch ~/.vault_pass.txt
+echo "Populate temp password file with values"
 echo $ENCRYPT_KEY >> ~/.vault_pass.txt
+echo "Decrypt vault file"
 ansible-vault decrypt vault.yml --vault-password-file ~/.vault_pass.txt
+echo "Create .env file"
 cp vault.yml .env
+echo "Re-encrypt vault file"
 ansible-vault encrypt vault.yml --vault-password-file ~/.vault_pass.txt
+echo "Remove temp password-file"
 rm ~/.vault_pass.txt
+echo "Start application"
 yarn start
-exit
 EOF
+exit
