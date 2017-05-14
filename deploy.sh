a7f840b37cdcaedacd025c62da23bbb2849176f6
Mark Arciaga <mark.arciaga@gmail.com>
2017-05-13 23:03:18 -0700

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
-echo 'deploy this business'
+#!/bin/bash
+
+ssh $REMOTE_IP 'bash -s < deploy.sh'
+
 echo $BUCKET_NAME
+
+# ssh into server
+# pull latest repo
+# run yarn install
+# consider whether new env vars need to be added
+# restart pm2
