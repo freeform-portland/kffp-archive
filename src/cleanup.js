ff8fa5151922b4d96ee105d6f757b6a3ab63e455
Mark Arciaga <mark.arciaga@gmail.com>
2018-07-14 11:12:45 -0700

    improve cleanup

--- src/cleanup.js
+++ src/cleanup.js
@@ -2,7 +2,7 @@
 // Upload files that aren't there
 import 'babel-polyfill';
 import dotenv from 'dotenv';
-import rimraf from 'rimraf';
+import del from 'del';
 import fs from 'fs';
 import path from 'path';
 import { uploadToS3, checkIfFileExistsOnS3 } from './upload';
@@ -11,16 +11,23 @@ dotenv.config();
 
 const PATH_TO_ARCHIVES = path.resolve(__dirname, '..', '..', '..', 'Archives');
 
-// delete all files in directory
-const deleteFiles = (path) => {
-    rimraf(path, {}, (err) => {
-        if (err) {
-            // consider reporting this
-            console.log('ERROR FROM rimfaf lib: ', err);
-        }
+const deleteFiles = async (p, filename) => {
+    // delete the files in the directory and not the directory itself
+    const dir = path.resolve(p, '*');
+    const pathsToDelete = [
+        dir,
+        `!${filename}`, // do not delete the file that's being written currently
+    ];
 
-        process.exit(0);
-    });
+    try {
+        const paths = await del(pathsToDelete);
+        console.log('Paths deleted: ', paths);
+    } catch (e) {
+        // consider reporting this
+        console.log('ERROR FROM deleting files: ', e);
+    }
+
+    process.exit(0);
 };
 
 const main = async () => {
@@ -28,10 +35,9 @@ const main = async () => {
         // read filenames from Archive dir
         const filenames = fs.readdirSync(PATH_TO_ARCHIVES);
         // we want to ignore the most recent file because the uploader will handle it
-        const constrainedFiles = filenames.slice(0, -1);
-
+        const fileToIgnore = filenames.pop();
         // async iterate and check whether the file exists on S3
-        for await (const file of constrainedFiles) {
+        for await (const file of filenames) {
             const fileExists = await checkIfFileExistsOnS3(file);
 
             if (!fileExists) {
@@ -39,9 +45,8 @@ const main = async () => {
                 await uploadToS3(`${PATH_TO_ARCHIVES}/${file}`);
             }
         }
-
         // clear out the directory
-        deleteFiles(PATH_TO_ARCHIVES);
+        deleteFiles(PATH_TO_ARCHIVES, fileToIgnore);
     } catch (e) {
         console.log('Something went wrong...', e);
     }
